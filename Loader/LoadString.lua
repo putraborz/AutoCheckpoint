6d5d0a0e4b4a304a655bda1224316423d062eabc
putraborz <putra.myapi@gmail.com>
2025-11-02 00:49:20 +0700

    Create LoadString.lua

---
+++ Loader/LoadString.lua
@@ -0,0 +1,15 @@
+
+---
+
+### 📄 `Loader/LoadString.lua`
+
+```lua
+--[[
+    Auto Checkpoint - LoadString Loader
+    GitHub: https://github.com/USERNAME/AutoCheckpoint
+]]
+
+local RepoURL = "https://raw.githubusercontent.com/putraborz/AutoCheckpoint/main/"
+
+-- Load Main Script
+loadstring(game:HttpGet(RepoURL .. "Loader/Main.lua"))()
