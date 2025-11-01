191d67fbd9ed17f4f68c731957e4e346a51b1341
putraborz <putra.myapi@gmail.com>
2025-11-02 00:45:17 +0700

    Create BasicUsage.lua

---
+++ Examples/BasicUsage.lua
@@ -0,0 +1,12 @@
+--[[
+    Basic Usage Example
+]]
+
+-- Simple LoadString
+loadstring(game:HttpGet("https://raw.githubusercontent.com/USERNAME/AutoCheckpoint/main/Loader/LoadString.lua"))()
+
+-- Enable Auto Checkpoint
+_G.AutoCheckpoint = true
+
+-- Enable Anchor Player
+_G.AnchorPlayer = true
