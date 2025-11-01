c2c0badab81a353f9f335eccb54ddc6dda863622
putraborz <putra.myapi@gmail.com>
2025-11-02 00:42:02 +0700

    Create Config.lua

---
+++ Source/Config.lua
@@ -0,0 +1,27 @@
+--[[
+    Configuration File
+]]
+
+return {
+    -- General Settings
+    Version = "1.0.0",
+    AutoUpdate = true,
+    
+    -- Checkpoint Settings
+    AutoClaim = false,
+    ClaimDelay = 0.1,
+    CheckpointFolder = "Checkpoints",
+    
+    -- Player Settings
+    AnchorPlayer = false,
+    SavePosition = true,
+    
+    -- GUI Settings
+    GUITheme = "Dark",
+    ShowNotifications = true,
+    DraggableGUI = true,
+    
+    -- Advanced
+    DebugMode = false,
+    SafeMode = true
+}
